fa8563a76fa0494e0afb165fdd658c4829ced4dd
Valerii Artemenko <87828092+ValeriiArt@users.noreply.github.com>
2022-07-09 18:21:35 +0300

    add searching images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,80 @@
 import React, { Component } from "react";
+import { ToastContainer } from 'react-toastify';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import 'react-toastify/dist/ReactToastify.css';
 import Modal from "./Modal";
+import Searchbar from "./Searchbar";
+import ImageGallery from "./ImageGallery";
+import Button from "./Button";
 
 export class App extends Component {
   state = {
     showModal: false,
+    searchText: '',
+    data: [],
+    page: 1,
+    modalImage:'',
+  };
 
-  }
+  handelFormSubmit = searchText => {
+    this.setState({
+            page: 1,
+            data: [],
+        });
+    this.setState({ searchText });
+  };
 
-  toggleModal = () => {
+  onChangeData = (data) => {
+    this.setState({
+      data: [...this.state.data, ...data],
+    });
+  };
+
+  loadMore = () => {
+    this.setState(prevState => ({
+        page: prevState.page + 1,
+    }))
+  };
+
+  toggleModal = (largeImageURL) => {
     this.setState(({ showModal }) => ({
       showModal: !showModal,
+      modalImage: largeImageURL,
     }));
-  }
+  };
+
+
   render() {
-    const { showModal } = this.state;
+    const { showModal, searchText, data, page, modalImage} = this.state;
     return (
       <>
-        <button type="button" onClick={this.toggleModal}>Open Modal</button>
-        {/* <Searchbar></Searchbar> */}
-        {/* <ImageGallery></ImageGallery> */}
-        {/* <ImageGalleryItem></ImageGalleryItem> */}
-        {/* <Loader></Loader> */}
-        {/* <Button></Button> */}
+        <Searchbar
+          onSubmit={this.handelFormSubmit}
+        />
+        <ImageGallery
+          searchText={searchText} 
+          data={data}
+          page={page} 
+          toggleModal={this.toggleModal}
+          onChangeData={this.onChangeData}
+        />
+
+        {this.state.data.length > 0 && <Button 
+          nameButton='Load More'
+          Click={this.loadMore}
+          />}
+        
         {showModal && (
           <Modal onClose={this.toggleModal}>
-            <button type="button" onClick={this.toggleModal}>Close Modal</button>
+            <img src={modalImage} alt="" />
+            <Button 
+              nameButton='Close'
+              Click={this.toggleModal}
+            />
           </Modal>
         )}
+
+        <ToastContainer autoClose={3000}/>
       </>
     ); 
   }
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -32,9 +32,7 @@ export default class Modal extends Component {
         return createPortal(
             <div className={s.Overlay} onClick={this.handleBackdropClick}>
                 <div className={s.Modal}>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem, soluta voluptatum praesentium voluptates voluptate ullam ad labore adipisci ipsa libero quam?
-                    Nisi perspiciatis rerum incidunt laudantium, quasi earum. Explicabo, voluptatum.
-                    <img src="" alt="" />
+                    
                     {this.props.children}
                 </div>
             </div>,
